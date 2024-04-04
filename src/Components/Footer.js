027077ab13aef181fa34540225081f128b8835c3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-04 05:32:17 +0200

    Create Footer.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+// assets
+import Logo from "../Assets/Logo.svg";
+
+// icons
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Footer;
