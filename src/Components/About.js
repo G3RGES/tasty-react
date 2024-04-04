2777f0981c45b8759d78fe59924ab3f06399c679
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-04 03:40:27 +0200

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,50 @@
 import React from "react";
 
+// assets
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+
+// icons
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-section-container" id="about">
+      <div className="about-background-image-container">
+        <img src={AboutBackground} alt="about_background" />
+      </div>
+
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+
+      <div className="about-section-text-container">
+        <p className="primary-subheading">ABOUT</p>
+
+        <h1 className="primary-heading">
+          Food Is An Important Part Of A Balanced Diet
+        </h1>
+
+        <p className="primary-text">
+          At FOODIE, we believe that food plays a crucial role in a healthy
+          lifestyle.
+        </p>
+
+        <p className="primary-text">
+          That's why we're committed to offering you delicious meals crafted
+          with quality ingredients, ensuring your dining experience is not only
+          enjoyable but also nourishing.
+        </p>
+
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
