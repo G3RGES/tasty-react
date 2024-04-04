8cb9b2e9de785a7452fd39f40d174547d5713758
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-04 02:16:36 +0200

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,12 +3,23 @@ import React from "react";
 // components
 import Navbar from "./Navbar";
 
+// assets
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+
+// icons
+import { FiArrowRight } from "react-icons/fi";
+
 const Home = () => {
   return (
     <div className="home-container">
       <Navbar />
 
-      <div className="home-banner-container">{/* to be continued */}</div>
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="home_banner_background" />
+        </div>
+      </div>
     </div>
   );
 };
