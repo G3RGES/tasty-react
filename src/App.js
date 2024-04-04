4e0aa50ff2156249adebc078f34f9ee7c8313e01
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-04-04 05:32:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import About from "./Components/About";
 import Home from "./Components/Home";
 import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
+import Footer from "./Components/Footer";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <About />
       <Work />
       <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
